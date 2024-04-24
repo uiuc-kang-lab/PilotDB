1e0311cd493a7acd12ae39f434ab9fbdfecb0ec7
Yuxuan Zhu <yxx404@illinois.edu>
2024-04-24 13:26:56 -0600

    immediate impl

--- pilotdb/pilot_engine/query_base.py
+++ pilotdb/pilot_engine/query_base.py
@@ -7,8 +7,10 @@ class Query():
     final_sample_query: str         # the approximate query with sampling
     pilot_query: str                # the approximate query with sampling and page statistics
     column_mapping: List[Dict]      # a list of {"aggregate": <aggregate>, "<statistics>": <column_name>}
-    res_2_page_id: Dict[str, str]   # the mapping from result column name to page id, i.e. {"r1": "page_id_0"}
     group_cols: List[str]           # a list of column names of original groups, 
                                     #   [] if the original query does not have group by
-    subquery_dict: Dict[str, str]   # a dictionary of subquery name and its query, i.e {"subquery_name": "subquery"}
+    page_size_col: str              # immediate impl, TOFIX
+    page_id_col: str                # immediate impl, TOFIX
+    subquery_dict: Dict[str, str]|None=None   # a dictionary of subquery name and its query, i.e {"subquery_name": "subquery"}
+    res_2_page_id: Dict[str, str]|None=None   # the mapping from result column name to page id, i.e. {"r1": "page_id_0"}
     
