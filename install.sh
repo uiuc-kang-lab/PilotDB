5ac2219eb85c8a65ee87256293a987e491e8ec1c
Yuxuan Zhu <yxx404@illinois.edu>
2024-04-30 22:49:42 -0400

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+# setup miniconda
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
@@ -5,9 +6,14 @@ rm -rf ~/miniconda3/miniconda.sh
 export PATH=$~/miniconda3/bin:$PATH
 ~/miniconda3/bin/conda init bash
 . ~/.bashrc
+
+# setup python environment
 conda create -y -n pilotdb
 conda activate pilotdb
 conda install -y -c conda-forge postgresql
-conda install -y python=3.10
+conda install -y python=3.11
 pip install -r requirements.txt
+gdown 1CXdBfgbef4AxZYjzzMYWaRADzHZ5vtAU
+pip install duckdb.whl
 pip install -e .
+rm duckdb.whl
