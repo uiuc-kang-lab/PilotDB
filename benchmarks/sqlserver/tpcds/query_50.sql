ce8c5f5b1364a2e1c9c9c5ff0b8a9b8678acd5f0
ttt-77 <1057858192@qq.com>
2024-05-08 01:04:37 -0500

    fix

--- benchmarks/sqlserver/tpcds/query_50.sql
+++ benchmarks/sqlserver/tpcds/query_50.sql
@@ -14,34 +14,34 @@ SELECT TOP 100
       WHEN (sr_returned_date_sk - ss_sold_date_sk <= 30) THEN 1
       ELSE 0
     END
-  ) AS "30 days",
+  ),
   sum(
     CASE
       WHEN (sr_returned_date_sk - ss_sold_date_sk > 30)
       AND (sr_returned_date_sk - ss_sold_date_sk <= 60) THEN 1
       ELSE 0
     END
-  ) AS "31-60 days",
+  ),
   sum(
     CASE
       WHEN (sr_returned_date_sk - ss_sold_date_sk > 60)
       AND (sr_returned_date_sk - ss_sold_date_sk <= 90) THEN 1
       ELSE 0
     END
-  ) AS "61-90 days",
+  ),
   sum(
     CASE
       WHEN (sr_returned_date_sk - ss_sold_date_sk > 90)
       AND (sr_returned_date_sk - ss_sold_date_sk <= 120) THEN 1
       ELSE 0
     END
-  ) AS "91-120 days",
+  ),
   sum(
     CASE
       WHEN (sr_returned_date_sk - ss_sold_date_sk > 120) THEN 1
       ELSE 0
     END
-  ) AS ">120 days"
+  )
 FROM store_sales,
   store_returns,
   store,
