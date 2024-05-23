60fca0d2d8ae9f18b02d1b54e603bc1d38a766f5
ttt-77 <tengjun2@illinois.edu>
2024-05-23 16:29:41 -0400

    add query

---
+++ benchmarks/sqlserver/tpcds/query_22.sql
@@ -0,0 +1,23 @@
+SELECT TOP 100
+    i_product_name,
+    i_brand,
+    i_class,
+    i_category,
+    avg(inv_quantity_on_hand) qoh
+FROM inventory,
+    date_dim,
+    item
+WHERE inv_date_sk = d_date_sk
+    AND inv_item_sk = i_item_sk
+    AND d_month_seq BETWEEN 1188 AND 1188 + 11
+GROUP BY rollup(
+        i_product_name,
+        i_brand,
+        i_class,
+        i_category
+    )
+ORDER BY qoh,
+    i_product_name,
+    i_brand,
+    i_class,
+    i_category;
