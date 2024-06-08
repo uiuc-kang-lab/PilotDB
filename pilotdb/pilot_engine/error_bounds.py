cf18e0e1a42300d505fc3d3878777f64d0f9454e
yuxuan <yuxuanzh@umich.edu>
2024-06-08 20:51:08 +0800

    special treatment for tpch1

--- pilotdb/pilot_engine/error_bounds.py
+++ pilotdb/pilot_engine/error_bounds.py
@@ -73,7 +73,7 @@ def get_sample_rate(fp: float, sample_size: int, pilot_sample_rate: float, pilot
 def get_bernoulli_N_sample_rate(error, fp: float, fp1: float, pilot_sample_rate: float, pilot_sample_size: int):
     bernoulli_N_lb = get_bernoulli_N_lb(pilot_sample_size, pilot_sample_rate, fp1)
     z_val = norm.ppf(1-fp/2)
-    return 1 / (1 + error**2 * bernoulli_N_lb**3 / z_val**2)
+    return 1 / (1 + error**2 * bernoulli_N_lb / z_val**2)
 
 def estimate_final_rate(failure_prob: float, pilot_results: pd.DataFrame, page_errors: Dict, group_cols: List[str], 
                         pilot_rate: float=0.0001, limit: int|None=None):
@@ -143,6 +143,25 @@ def estimate_final_rate_uniform(failure_prob: float, pilot_results: pd.DataFrame
                 final_sample_size = get_mean_sample_size(error, failure_prob, failure_prob, pilot_rate, sample_mean, sample_std, sample_size)
                 final_sample_rate = get_sample_rate(failure_prob, final_sample_size, pilot_rate, sample_size)
             max_sample_rate = max(max_sample_rate, final_sample_rate)
+    return max_sample_rate
+
+def estimate_final_rate_oracle_tpch1(pilot_results: pd.DataFrame):
+    columns = ["avg_1", "avg_2", "avg_3", "avg_4", "avg_5"]
+    max_sample_rate = 0
+    n_groups = len(pilot_results)
+    fp = 1 - math.pow(1-0.05, 1/n_groups/len(columns)/4)
+    for row in pilot_results.iterrows():
+        for col in columns:
+            sample_mean = row[col]
+            sample_std = row[col.replace("avg", "std")]
+            sample_size = row["n_page"]
+            final_sample_size = get_mean_sample_size(0.024, fp, fp, fp, sample_mean, sample_std, sample_size)
+            final_sample_rate = get_sample_rate(fp, final_sample_size, 1, sample_size)
+            max_sample_rate = max(max_sample_rate, final_sample_rate)
+        # sample rate for size column
+        sample_size = row["n_page"]
+        final_sample_rate = get_bernoulli_N_sample_rate(0.024, fp, fp, 1, sample_size)
+        max_sample_rate = max(max_sample_rate, final_sample_rate)
     return max_sample_rate
 if __name__ == "__main__":
     print(get_sample_rate(0.025, 300, 0.0001, 30))
