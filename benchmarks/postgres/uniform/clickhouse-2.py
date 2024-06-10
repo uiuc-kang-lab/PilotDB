d6dcef96431e65c6b334d710cf59b8a6602b2dcd
ttt-77 <tengjun2@illinois.edu>
2024-06-10 17:27:08 -0400

    add clickhouse

---
+++ benchmarks/postgres/uniform/clickhouse-2.py
@@ -0,0 +1,11 @@
+pilot_query = """
+SELECT COUNT(*) as sample_size
+FROM hits{sampling_method}
+WHERE AdvEngineID <> 0;
+"""
+
+results_mapping = [
+    {"aggregate": "count", "size": "sample_size"}
+]
+
+subquery_dict = []
